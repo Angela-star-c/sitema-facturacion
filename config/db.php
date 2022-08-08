86e19b78602af7ceaed2b9b949d3309e065c70c7
Angela-star-c <angelescalla.b@gmail.com>
2022-08-07 20:29:29 -0500

    subiendo aplicacion de facturacion.

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'sql5.freesqldatabase.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'sql5511425');//Usuario de tu base de datos
+define('DB_PASS', 'pICKQH1DDJ');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'sql5511425');//Nombre de la base de datos
+?>
